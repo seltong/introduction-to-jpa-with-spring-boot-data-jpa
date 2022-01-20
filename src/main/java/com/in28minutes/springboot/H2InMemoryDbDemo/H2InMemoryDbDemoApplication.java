7fc4c70fdffaa196999a99506cacacb57a39a729
Selton Guedes <sfgs@ecomp.poli.br>
2022-01-20 12:14:30 -0300

    Initial commit

---
+++ src/main/java/com/in28minutes/springboot/H2InMemoryDbDemo/H2InMemoryDbDemoApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.H2InMemoryDbDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2InMemoryDbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2InMemoryDbDemoApplication.class, args);
+	}
+
+}
